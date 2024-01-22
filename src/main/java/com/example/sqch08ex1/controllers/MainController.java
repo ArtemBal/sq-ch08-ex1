77e0abb31d8622c57ee15527c2cbce997a97e127
Artem Balan <a.s.balan96@gmail.com>
2024-01-22 22:05:59 +0200

    add request parameters

--- src/main/java/com/example/sqch08ex1/controllers/MainController.java
+++ src/main/java/com/example/sqch08ex1/controllers/MainController.java
@@ -3,14 +3,18 @@ package com.example.sqch08ex1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Artem");
-        page.addAttribute("color", "red");
+    public String home(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String color,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
